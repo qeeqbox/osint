dea8ad19147321821c4081345ea9403a3a3bbdd3
G <52905881+giga-a@users.noreply.github.com>
2021-04-20 12:37:17 -0700

    Create auto_pip.sh

---
+++ auto_pip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Auto pip Package Creator - QeeqBox"
+echo "[x] Creating pypi Package"
+python3 setup.py sdist bdist_wheel 2>stderr.log 1>stdout.log
+if grep -q "error:" stderr.log
+then
+	echo "[x] Creating pypi failed.."
+	cat stderr.log
+else
+	echo "[x] pypi Package was created successfully"
+	read -p "Do you want to upload? (y/n)?" USER_INPUT
+	if [ "$USER_INPUT" = "y" ]; then
+		echo "[x] Uploading pypi Package"
+		twine upload dist/*
+	fi
+fi
